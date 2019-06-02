54da9dd9047f71eeb2badac6716d79a41d670a90
Uma Zalakain <uma@gisa-elkartea.org>
2019-06-02 22:51:15 +0100

    Get parametric message types finally working

--- st.v
+++ st.v
@@ -8,6 +8,7 @@ ISSUES:
 [x] How to avoid passing channel and message to every process constructor
 [x] How to send actual messages
 [ ] Add pretty notation
+[ ] Evaluate processes
 *)
 
 (* Represents the type of a message.
@@ -42,13 +43,16 @@ Inductive Duality : SType → SType → Prop :=
    For some reason, going universe polymorphic with
    (A : Type) {M : A} (m : M) is not working.
 *)
-Inductive Message {channel : SType → Type} : MType → Type :=
+Inductive Message {CT : SType → Type} : MType → Type :=
 | V : ∀ {M : Set}, M → Message (Base M)
-| C : ∀ {S : SType}, channel S → Message (Channel S)
+| C : ∀ {S : SType}, CT S → Message (Channel S)
 .
   
 (* Parametric HOAS representation of processes. *)
-Inductive Process {channel : SType → Type} : Type := 
+(* A polymorphic CT makes it impossible to produce new channels unles PNew is used *)
+(* MT and f allow for all messages to be mapped into the unit type *)
+(* This allows for the process to be traversed without evaluating it *)
+Inductive Process {CT : SType → Type} {MT : MType → MType} {f : ∀ m, Message (CT := CT) m → Message (CT := CT) (MT m)}: Type := 
 
 (* For any two dual session types,
    create their corresponding channels, and
@@ -57,7 +61,7 @@ Inductive Process {channel : SType → Type} : Type :=
 | PNew
   : ∀ (s r : SType)
   , Duality s r
-  → (channel s → channel r → Process)
+  → (CT s → CT r → Process)
   → Process
 
 (* Await a message m and continue with
@@ -68,8 +72,8 @@ Inductive Process {channel : SType → Type} : Type :=
 *)
 | PInput
   : ∀ {m : MType} {c : SType}
-  , (Message (channel:=channel) m → channel c → Process)
-  → (channel (Receive m c))
+  , (Message (CT := CT) (MT m) → CT c → Process)
+  → (CT (Receive m c))
   → Process
 
 (* Send a message m and continue with
@@ -80,44 +84,57 @@ Inductive Process {channel : SType → Type} : Type :=
 *)
 | POutput
   : ∀ {m : MType} {c : SType}
-  , Message (channel:=channel) m
-  → (channel c → Process)
-  → (channel (Send m c))
+  , Message (CT := CT) (MT m)
+  → (CT c → Process)
+  → (CT (Send m c))
   → Process
 
 (* I don't know how to represent this accurately, yet. *)
 | PComp : Process → Process → Process
 
 (* The end of all things, provided we are allowed to end. *)
-| P0 : channel End → Process
+| P0 : CT End → Process
 .
 
 (* Counts the instantiated variables.
-   Processes are parametrised with channels represented by unit, which is always available.
+   Channels are cast into unit, which is always available.
+   Message types are cast into unit, which is always available.
    It is in this way always possible to "dive in" into processes recursively.
-   The only problem are message types, which would also have to be restricted to unit.
 *)
-Fixpoint count_vars (p : Process (channel := fun _ => unit)) : nat :=
+Fixpoint count_vars (p : Process (CT := fun _ => unit) (MT := fun _ => Base unit) (f := fun _ _ => (V tt))) : nat :=
   match p with
   | PNew _ _ _ p => 2 + count_vars (p tt tt)
-  | PInput p _ => 1 (* + count_vars (p tt tt) *)
+  | PInput p _ => 1 + count_vars (p (V tt) tt)
   | POutput _ p _ => count_vars (p tt)
   | PComp p1 p2 => count_vars p1 + count_vars p2
   | P0 _ => 0
   end.
 
-(* Transform the datatype parameter into a quantifier *)
-Definition P := ∀ (c : SType → Type), Process (channel := c).
+(* How to decide linearity of processes
+   ———————————————————————————————————–
 
-Example foo : P := fun _ => (* This process can be instantiated to whatever channel representation *)
+   - Parametrise process by channel := fun _ => nat
+     (or any other infinite set with decidable equality)
+   - Keep a set of nats representing channels that have been created
+   - Keep counter of natural of latest created channel
+   - When going into a PNew, add new naturals and increment counter
+   - When going into a send, receive or end:
+     - If the channel num they are using is in the set, remove it
+     - If it is not, it is not linear
+*)
+
+(* Transform the datatype parameter into a function *)
+Definition P := ∀ (CT : SType → Type) (MT : MType → MType) (f : ∀ m, Message (CT := CT) m → Message (CT := CT) (MT m)) , Process (CT := CT) (MT := MT) (f := f). 
+
+Example foo : P := fun _ _ f => (* This process can be instantiated to whatever channel and message types *)
         PNew
           (* Session types for both ends *)
           (* Pertinent notation will make this nicer to write *)
           (* NOTABLY: if you change any of this, this process won't typecheck *)
-          (Receive (Base bool) End)
-          (Send (Base bool) End)
+          (Receive (Base bool) (Send (Base bool) End))
+          (Send (Base bool) (Receive (Base bool) End))
           (* This is going to be computed automatically in the future *)
-          (Leftwards Ends)
+          (Leftwards (Rightwards Ends))
 
           (* The magic of parametric HOAS: *)
           (* i and o represent channels with pertinent session types *)
@@ -125,10 +142,13 @@ Example foo : P := fun _ => (* This process can be instantiated to whatever chan
                (* Some useful typechecking should happen here *)
                PComp
                   (* Here m is the message upon arrival! *)
-                  (PInput (fun m => P0) i)
+                  (PInput (fun m => POutput m P0) i)
                   (* We send the value 1 over the wire. *)
-                  (POutput (V true) P0 o)
+                  (* We use f whenever we fabricate a value
+                     so that it is cast into the relevant value. *)
+                  (POutput (f _ (V true)) (PInput (fun m => P0)) o)
           ).
 
-(* We compute the amount of variables in this process: 3! *)
-Compute count_vars (foo _).
+(* We compute the amount of variables in this process:
+   2 channels, 2 inputs, total of 4! *)
+Compute count_vars (foo _ _ _).
