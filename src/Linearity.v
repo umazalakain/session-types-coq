1bbf4611bcdbf6b59396d78a75aad69761d2fbcf
Uma Zalakain <uma@gisa-elkartea.org>
2019-08-24 15:56:25 +0200

    Make the linearity predicate easier to read

--- src/Linearity.v
+++ src/Linearity.v
@@ -17,27 +17,21 @@ Derive NoConfusion for MType.
 Derive Signature for Message.
 
 Equations lin (P : Process bool TMT) : Prop := {
-lin (PEnd (C true))  := False;
-lin (PEnd (C false)) := True;
-
-lin (PNew _ _ _ P) => single_x (P x o) /\ single_x (P o x) /\ lin (P o o) ;
-
-lin (PComp P Q) => lin P /\ lin Q ;
-
-lin (@PInput _    _ P (C true))  := False;
-lin (@PInput B[_] _ P (C false)) := single_x (P (V tt) x) /\ lin (P (V tt) o);
-lin (@PInput C[_] _ P (C false)) := single_x (P x o) /\ single_x (P o x) /\ lin (P o o);
-
+lin (PEnd (C true))                := False;
+lin (PEnd (C false))               := True;
+lin (PNew _ _ _ P)                 := lin (P o o) /\ single_x (P x o) /\ single_x (P o x);
+lin (PComp P Q)                    := lin P /\ lin Q ;
+lin (@PInput _    _ P (C true))    := False;
+lin (@PInput B[_] _ P (C false))   := lin (P (V tt) o) /\ single_x (P (V tt) x);
+lin (@PInput C[_] _ P (C false))   := lin (P o o) /\ single_x (P x o) /\ single_x (P o x);
 lin (POutput _        P (C true))  := False;
 lin (POutput (C true) P (C false)) := False;
-lin (POutput _        P (C false)) := single_x (P x) /\ lin (P o);
-
-lin (PSelect _ c P) with c := {
-| (C true) => False ;
-| (C false) => single_x (P x) /\ lin (P o)};
-
-lin (PBranch Ps (C true))  := False;
-lin (PBranch Ps (C false)) :=
+lin (POutput _        P (C false)) := lin (P o) /\ single_x (P x);
+lin (PSelect _ c P) with c         := {
+lin (PSelect _ c P) (C true)       := False;
+lin (PSelect _ c P) (C false)      := lin (P o) /\ single_x (P x)};
+lin (PBranch Ps (C true))          := False;
+lin (PBranch Ps (C false))         :=
   let fix branches {n} {ss : Vector.t _ n} (Ps : Forall (fun s => _ -> _) ss) :=
       match Ps with
       | Forall_nil _ => True
@@ -50,46 +44,31 @@ lin (PBranch Ps (C false)) :=
 
 } where single_x (p : Process bool TMT) : Prop := {
 
-single_x (PEnd (C true)) := True;
-single_x (PEnd (C false)) := False;
-
-single_x (PNew _ _ _ P) => single_x (P o o) ;
-
-single_x (PComp P Q) := (lin P /\ single_x Q) \/ (single_x P /\ lin Q) ;
-
-single_x (@PInput B[_] _ P (C true))  := lin (P (V tt) o) /\ single_x (P (V tt) x);
-single_x (@PInput C[_] _ P (C true))  := lin (P o o) /\ single_x (P x o) /\ single_x (P o x);
-single_x (@PInput B[_] _ P (C false)) := single_x (P (V tt) o) ;
-single_x (@PInput C[_] _ P (C false)) := single_x (P o o);
-
+single_x (PEnd (C true))                := True;
+single_x (PEnd (C false))               := False;
+single_x (PNew _ _ _ P)                 := single_x (P o o) ;
+single_x (PComp P Q)                    := (lin P /\ single_x Q) \/ (single_x P /\ lin Q) ;
+single_x (@PInput B[_] _ P (C true))    := lin (P (V tt) o) /\ single_x (P (V tt) x);
+single_x (@PInput C[_] _ P (C true))    := lin (P o o) /\ single_x (P x o) /\ single_x (P o x);
+single_x (@PInput B[_] _ P (C false))   := single_x (P (V tt) o) ;
+single_x (@PInput C[_] _ P (C false))   := single_x (P o o);
 single_x (POutput (C true) P (C true))  := False;
 single_x (POutput _        P (C true))  := lin (P o) /\ single_x (P x);
 single_x (POutput (C true) P (C false)) := lin (P o) /\ single_x (P x) ;
 single_x (POutput _        P (C false)) := single_x (P o);
-
-single_x (PSelect i c P) with c => {
-| (C true) => lin (P o) /\ single_x (P x) ;
-| (C false) => single_x (P o)};
-
-single_x (PBranch Ps (C true))  :=
+single_x (PSelect i c P) with c         := {
+single_x (PSelect i c P) (C true)       := lin (P o) /\ single_x (P x) ;
+single_x (PSelect i c P) (C false)      := single_x (P o)};
+single_x (PBranch Ps (C m))             :=
   let fix branches {n} {ss : Vector.t _ n} (Ps : Forall (fun s => _ -> _) ss) :=
       match Ps with
       | Forall_nil _ => True
       | Forall_cons _ P Ps' =>
-        lin (P o) /\
-        single_x (P x) /\
-        branches Ps'
-      end
-  in branches Ps ;
-single_x (PBranch Ps (C false)) :=
-  let fix branches {n} {ss : Vector.t _ n} (Ps : Forall (fun s => _ -> _) ss) :=
-      match Ps with
-      | Forall_nil _ => True
-      | Forall_cons _ P Ps' =>
-        single_x (P o) /\
-        branches Ps'
+        (if m then lin (P o) /\ single_x (P x)
+              else single_x (P o))
+        /\ branches Ps'
       end
   in branches Ps
 }.
 
-Definition Linear (p : PProcess) : Prop := lin (p bool TMT fMT).
+Definition Linear (P : PProcess) : Prop := lin (P bool TMT fMT).
